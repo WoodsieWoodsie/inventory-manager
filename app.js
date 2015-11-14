0eaa616697b6ee5c914d906c213d28f753810431
Rachel Wood <r@codinghouse.co>
2015-11-14 09:24:55 -0800

    Ready for heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/itemsmanager');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/itemsmanager');
 
 var app = express();
 
--- readme.md
+++
@@ -1,3 +0,0 @@
-# Express Example
-
-First, cd into the project directory, and do `npm run setup` to install the dependencies listed in the package.json and bower.json.  Then you can start the server in Node.
